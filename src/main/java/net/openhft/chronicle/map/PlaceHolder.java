39fbb532ccdbd1c6357760e49a380593dcc8cf1e
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-11-27 18:06:03 +0000

    HCOLL-220 added place holder

---
+++ src/main/java/net/openhft/chronicle/map/PlaceHolder.java
@@ -0,0 +1,11 @@
+package net.openhft.chronicle.map;
+
+
+public class PlaceHolder {
+
+    // just created as a place holder as the javadoc is failing with public classes
+    // can be removed once we have created public classes
+
+    public PlaceHolder() {
+    }
+}
